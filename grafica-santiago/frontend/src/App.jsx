98ac507637be9ac207862c6c62945ac55664f229
ANTONELLA DEL CISNE PARRA CARRION <adparra2@utpl.edu.ec>
2026-01-19 09:16:52 -0500

    Cambios de ip

--- grafica-santiago/frontend/src/App.jsx
+++ grafica-santiago/frontend/src/App.jsx
@@ -13,7 +13,7 @@ import { Logo } from './components/Logo';
 // ==========================================
 // CONFIGURACIÓN API
 // ==========================================
-const API_URL = 'http://10.155.87.190:3000/api/v1'; 
+const API_URL = 'http://172.18.93.23:3000/api/v1';
 
 // ==========================================
 // 1. CONTEXTO (LOGIC LAYER)
